ac0ee11915d45e51d8b4ddd02ea37f453304457b
Augusto da Cunha Paris <augustoparis16@hotmail.com>
2015-04-09 17:17:56 -0300

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   //if(window.location.href=="http://blog.ankanbiswas.in/uno-dbyll/") {
 
   //$("#sidebar").css({width:'100%'});
-
+  console.log('app.js');
   $("#btnblog").click(function(){
     $("#sidebar").animate({width:'33.3333%'},'slow');
   });
