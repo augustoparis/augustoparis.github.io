150f8c5e2ce20828fa223d8dc714835e6724a0d4
Augusto da Cunha Paris <augustoparis16@hotmail.com>
2015-04-09 17:42:58 -0300

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   //alert("hello world");
-  if(window.location.href=="http://augustoparis.github.io") {
+  if(window.location.href=="http://augustoparis.github.io/") {
 
   $("#sidebar").css({width:'100%'});
 
